709a48cd32e4fdc94f12a6a19047a83a7ac2a7e2
Universitario <seg.wild07@gmail.com>
2018-09-20 19:09:39 -0400

    1º parte de mario

---
+++ Practica Mario/Assets/Scripts/MovPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPlayer : MonoBehaviour {
+
+    public float speed = 10.0F; //Velocidad de movimiento
+    //public float rotationSpeed = 100.0F; //Velocidad de rotación
+
+    void Update()
+    {
+        transform.Translate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
+        //transform.Rotate(0, Input.GetAxis("Vertical") * rotationSpeed * Time.deltaTime, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemigo")
+        {
+            //Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
